9e95dd3d75a1fae88825d8d40dba7aafbefbeb6a
tomaszociepa <tom.ociepa@gmail.com>
2019-06-03 23:26:32 +0200

    exercise 2 table

--- src/main/java/pl/infoshare/structures/_2_lists/_1_array/ArrayExample.java
+++ src/main/java/pl/infoshare/structures/_2_lists/_1_array/ArrayExample.java
@@ -1,5 +1,7 @@
 package main.java.pl.infoshare.structures._2_lists._1_array;
 
+import main.java.pl.infoshare.structures._1_pojo.Person;
+
 public class ArrayExample {
 
     // TODO: Utwórz tablicę zawierającą 3 różne osoby
@@ -8,6 +10,11 @@ public class ArrayExample {
     // TODO: Wypisz czwartą osobę na ekran
     // TODO: Dodaj 2 kolejne osoby do tablicy
     public static void main(String[] args) {
+        Person[] persons = new Person[3];
+        persons[0] = new Person("Tomek","Ociepa", "Gdańsk", 29);
+        persons[1] = new Person("Klaudia","Quirini", "Gdańsk", 24);
+        persons[2] = new Person("Zinedine","Zidane", "Madryt", 46);
 
+        System.out.println(persons[1]);
     }
 }
