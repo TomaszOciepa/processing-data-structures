71736d498a10ebc17c3dd8bc09d1e82e7e78f6f6
tomaszociepa <tom.ociepa@gmail.com>
2019-06-07 19:58:32 +0200

    exercise3 mapsExercise1

--- src/main/java/pl/infoshare/structures/_3_maps/_3_exercise/MapsExercise1.java
+++ src/main/java/pl/infoshare/structures/_3_maps/_3_exercise/MapsExercise1.java
@@ -1,5 +1,8 @@
 package main.java.pl.infoshare.structures._3_maps._3_exercise;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MapsExercise1 {
 
     // TODO: Ćwiczenie 3 - Map API
@@ -12,6 +15,31 @@ public class MapsExercise1 {
     // TODO: 7. Wypisz na ekran tylko ilość mieszkańców
     // TODO: 8. Wypisz na ekran dane dla Gdańska, Wejherowa i Warszawy. Jeżeli miasta nie ma wypisz zero.
     public static void main(String[] args) {
+
+        Map<String, Integer> cityPopulation = new HashMap<>(); // 1
+
+        cityPopulation.put("Gdańsk", 400000); //2
+        cityPopulation.put("Gdynia", 300000); //2
+        cityPopulation.put("Sopot", 200000); //2
+
+        cityPopulation.put("Gdynia", 150); //3
+
+        System.out.println("get "+cityPopulation.get("Gdynia")); //3
+
+        cityPopulation.putIfAbsent("Wejcherowo", 36000); //4
+        System.out.println("get "+cityPopulation.get("Wejcherowo")); //4
+
+        cityPopulation.putIfAbsent("Gdańsk", 100000); //5
+        System.out.println("get "+cityPopulation.get("Gdańsk")); //5
+
+        System.out.println("keySet "+cityPopulation.keySet()); //6
+        System.out.println("values "+cityPopulation.values()); //7
+
+        System.out.println("getOrDefault "+ cityPopulation.getOrDefault("Gdańsk", 0));
+        System.out.println("getOrDefault "+ cityPopulation.getOrDefault("Wejcherowo", 0));
+        System.out.println("getOrDefault "+ cityPopulation.getOrDefault("Warszawa", 0));
+
+
 
     }
 }
